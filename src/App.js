7ac68f701ad6f6b9009d3a2ac854b839aa56d721
MdGolamMostafa <shakilhero11@gmail.com>
2020-10-30 14:15:50 +0600

    start with nav

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import My_Navbar from './components/Navbar/Navbar.jsx'
+function App() {
+  return (
+    (<div className="App">
+      <My_Navbar></My_Navbar>
+      bismillah
+    </div>)
+  );
+}
+
+export default App;
