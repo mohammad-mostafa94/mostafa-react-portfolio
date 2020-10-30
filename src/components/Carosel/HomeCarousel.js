6e6f2cd2a86d316b05aa36b1d9a6e32bd72df4ce
MdGolamMostafa <shakilhero11@gmail.com>
2020-10-30 16:55:31 +0600

    go down  added

--- src/components/Carosel/HomeCarousel.js
+++ src/components/Carosel/HomeCarousel.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Carousel from "react-bootstrap/Carousel";
+import ScrollDown from '../GoDown/GoDown';
 import './carousel.css'
 const HomeCarousel = () => {
     return (
@@ -15,7 +16,7 @@ const HomeCarousel = () => {
                     <img className="d-block w-100 custom-img" src={'https://i.ibb.co/FgS8KMZ/react-carosel.jpg'} alt="react slide" />
                 </Carousel.Item>
             </Carousel>
-      {/* <ScrollDown /> */}
+      <ScrollDown />
     </div>
     );
 };
