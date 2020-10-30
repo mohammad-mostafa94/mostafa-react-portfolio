69f218506b132cb87c7a2f483d9638a7dd74ea0b
MdGolamMostafa <shakilhero11@gmail.com>
2020-10-30 16:43:01 +0600

    title message added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import HomeCarousel from './components/Carosel/HomeCarousel';
+import TitleMessage from './components/MyTitleMessage/TitleMessage';
 import MyNavbar from './components/Navbar/Navbar.jsx'
 function App() {
   return (
     (<div className="App">
       <MyNavbar/>
+      <TitleMessage></TitleMessage>
       <HomeCarousel></HomeCarousel>
      
     </div>)
--- src/components/Carosel/HomeCarousel.js
+++ src/components/Carosel/HomeCarousel.js
@@ -5,12 +5,12 @@ const HomeCarousel = () => {
     return (
         <div id="home">
             <Carousel controls={false} indicators interval={2500} pause={false}>
-                <Carousel.Item>
+                {/* <Carousel.Item>
                     <img className="d-block w-100 custom-img" src={'https://i.ibb.co/vdyr85T/programing-Language.jpg'} alt="technology slide" />
                 </Carousel.Item>
                 <Carousel.Item>
                     <img className="d-block w-100 custom-img" src={'https://i.ibb.co/D1Vdnff/programing-Language-One.jpg'} alt="programig slide" />
-                </Carousel.Item>
+                </Carousel.Item> */}
                 <Carousel.Item>
                     <img className="d-block w-100 custom-img" src={'https://i.ibb.co/FgS8KMZ/react-carosel.jpg'} alt="react slide" />
                 </Carousel.Item>
